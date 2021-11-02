f112118b8ec3367d2ba9bb78ca4d08692e20c450
Fasihuddin <syedfasih@outlook.com>
2021-11-02 12:21:48 +1100

    Update Python files

---
+++ LabelPowerset.py
@@ -0,0 +1,15 @@
+# using Label Powerset
+from skmultilearn.problem_transform import LabelPowerset
+from sklearn.naive_bayes import GaussianNB
+
+# initialize Label Powerset multi-label classifier
+# with a gaussian naive bayes base classifier
+classifier = LabelPowerset(GaussianNB())
+
+# train
+classifier.fit(X_train, y_train)
+
+# predict
+predictions = classifier.predict(X_test)
+
+accuracy_score(y_test,predictions)
